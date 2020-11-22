214c61383c2e72c47900e1a5e3cf6241651ed64a
Nikolas Laus <49353043+blinchk@users.noreply.github.com>
2020-11-22 22:59:13 +0200

    Добавьте файлы проекта.

---
+++ tthk-xamarin-carousel/MainPage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace tthk_xamarin_carousel
+{
+	public class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+
+        }
+    }
+}
---
+++ tthk-xamarin-carousel/TemperPage.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace tthk_xamarin_carousel
+{
+	public class TemperPage : ContentPage
+    {
+        private Image tempImage;
+        private Label tempTitleLabel, tempDescriptionLabel;
+        private Button tempTestButton;
+        private string tempTestUrl;
+        public TemperPage()
+        {
+            tempImage = new Image();
+            tempTestButton.Clicked += TempTestButtonOnClicked;
+            FlexLayout flexLayout = new FlexLayout();
+        }
+
+        public string TemperTitle
+        {
+            get
+            {
+                return tempTitleLabel.Text;
+            }
+            set
+            {
+                tempTitleLabel.Text = value;
+            }
+        }
+
+        public string TemperDescription
+        {
+            get
+            {
+                return tempDescriptionLabel.Text;
+            }
+            set
+            {
+                tempDescriptionLabel.Text = value;
+            }
+        }
+
+        private void TempTestButtonOnClicked(object sender, EventArgs e)
+        {
+            Browser.OpenAsync(tempTestUrl);
+        }
+    }
+}
